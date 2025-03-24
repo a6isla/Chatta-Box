3a6525651f29d3d31104f69a409a4e9dfcd79f86
127.0.0.1 <curtiskanga2021@gmail.com>
2025-03-24 12:28:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 from flask_socketio import SocketIO, send
+import datetime
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Index route renders the chat page
+# Store messages for new users to see past messages
+messages = []
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Listen for messages from clients and broadcast them to everyone in the room
 @socketio.on('message')
 def handle_message(msg):
-    send(msg, broadcast=True)
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    formatted_msg = {'username': session.get('username', 'Anonymous'), 'text': msg, 'time': timestamp}
+    messages.append(formatted_msg)
+    send(formatted_msg, broadcast=True)
+
+@socketio.on('connect')
+def handle_connect():
+    send({'messages': messages}, json=True)
+
+@app.route('/set_username', methods=['POST'])
+def set_username():
+    session['username'] = request.form['username']
+    return "", 204
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000)
